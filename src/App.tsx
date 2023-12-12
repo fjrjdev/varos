f0f66eac1355e997bd44dd3e5821aa6bc858a1dd
Fabio Junior <thedromemafia@gmail.com>
2023-12-12 09:40:06 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import TestComponent from './components/TestComponent'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <TestComponent />
+      </div>
+    </>
+  )
+}
+
+export default App
