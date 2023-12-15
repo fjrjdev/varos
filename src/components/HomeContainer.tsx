b5200c7a96e063f59d2c4bda4e00d54f5ca58604
Fabio Junior <thedromemafia@gmail.com>
2023-12-15 17:35:24 -0300

    style: home container gap adjust

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -6,7 +6,7 @@ const HomeContainer = () => {
     return (
         <div className="max-w-7xl mx-auto  px-4 py-4 sm:px-6 lg:px-8 bg-[#131313]">
             <div className="flex ">
-                <div className="flex flex-1 flex-col gap-4">
+                <div className="flex flex-1 flex-col gap-8">
                     <div className="flex flex-col gap-6">
                         <h1 className="text-[#F2F4F8] text-custom-mobile sm:text-custom-tablet lg:text-custom-desktop leading-tight">
                             Investir de forma mais inteligente passa por aqui.
