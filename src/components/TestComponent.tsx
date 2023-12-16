24e4957980db4b5d999727124118883bba441301
Fabio Junior <thedromemafia@gmail.com>
2023-12-16 11:02:34 -0300

    delete: removed test component

--- src/components/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-const TestComponent = () => {
-    return (
-        <div className="bg-blue-500 text-black p-4">
-            Este é um componente de teste com Tailwind CSS!
-        </div>
-    );
-};
-
-export default TestComponent
